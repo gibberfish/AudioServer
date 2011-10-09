4968ffa6fc347ca0f5cbcd17b50b5607a4474200
Mark Fowler <growlerfowler@talktalk.net>
2011-10-09 21:37:16 +0100

    Added servlet to return library last updated date

---
+++ src/com/mindbadger/spring/controller/GetLibraryLastUpdateDateController.java
@@ -0,0 +1,43 @@
+package com.mindbadger.spring.controller;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mindbadger.audioserver.schema.AudioserverDocument;
+import com.mindbadger.library.Librarian;
+
+@Controller
+public class GetLibraryLastUpdateDateController {
+
+  @Autowired
+  private Librarian librarian;
+  
+  @RequestMapping("/getLastUpdateDate")
+  public ModelAndView getLibraryLastUpdateDate(HttpServletRequest request, HttpServletResponse response) {
+    try {
+      AudioserverDocument doc = librarian.getXml();
+      
+      Calendar calendar = doc.getAudioserver().getLastUpdated();
+      final String DATE_FORMAT = "dd-MM-yyyy HH:mm:ss";
+      String dateString = (new SimpleDateFormat(DATE_FORMAT)).format(calendar.getTime());
+      
+      ServletOutputStream outputStream = response.getOutputStream();
+      outputStream.print(dateString);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+    return null;
+  }
+
+}
