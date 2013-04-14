7a67c2f0a29e84ac1fc41e225c0587923a3f7ae7
Mark Fowler <growler1970@gmail.com>
2013-04-14 15:36:37 +0100

    logging added

--- src/main/java/com/mindbadger/wmp/JacobAdapter.java
+++ src/main/java/com/mindbadger/wmp/JacobAdapter.java
@@ -1,34 +1,40 @@
 package com.mindbadger.wmp;
 
+import org.apache.log4j.Logger;
+
 import com.jacob.activeX.ActiveXComponent;
 import com.jacob.com.Dispatch;
 import com.jacob.com.Variant;
 
 public class JacobAdapter {
-  private ActiveXComponent wmp;
-  
-  public JacobAdapter () {
-    // Need to ensure the DLL is installed on the target PC
-    wmp = new ActiveXComponent("WMPlayer.OCX");
-  }
-  
-  public Dispatch getDispatch (String property) {
-    return wmp.getProperty("mediaCollection").toDispatch();
-  }
-  
-  public Variant getVariant (Dispatch dispatch, String property) {
-    return Dispatch.get(dispatch, property); 
-  }
+	Logger logger = Logger.getLogger(JacobAdapter.class);
 
-  public Dispatch call (Dispatch itemToExecuteOn, String method) {
-    return Dispatch.call(itemToExecuteOn, method).toDispatch();
-  }
+	private ActiveXComponent wmp;
 
-  public Dispatch call(Dispatch itemToExecuteOn, String method, Object arg) {
-    return Dispatch.call(itemToExecuteOn, method, arg).toDispatch();
-  }
-  
-  public Variant getVariant(Dispatch itemToExecuteOn, String method, Object arg) {
-    return Dispatch.call(itemToExecuteOn, method, arg);
-  }
+	public JacobAdapter() {
+		// Need to ensure the DLL is installed on the target PC
+		wmp = new ActiveXComponent("WMPlayer.OCX");
+		
+		logger.debug("WMPlayer.OCX: " + wmp);
+	}
+
+	public Dispatch getDispatch(String property) {
+		return wmp.getProperty("mediaCollection").toDispatch();
+	}
+
+	public Variant getVariant(Dispatch dispatch, String property) {
+		return Dispatch.get(dispatch, property);
+	}
+
+	public Dispatch call(Dispatch itemToExecuteOn, String method) {
+		return Dispatch.call(itemToExecuteOn, method).toDispatch();
+	}
+
+	public Dispatch call(Dispatch itemToExecuteOn, String method, Object arg) {
+		return Dispatch.call(itemToExecuteOn, method, arg).toDispatch();
+	}
+
+	public Variant getVariant(Dispatch itemToExecuteOn, String method, Object arg) {
+		return Dispatch.call(itemToExecuteOn, method, arg);
+	}
 }
--- src/main/java/com/mindbadger/wmp/ReadWMPLibrary.java
+++ src/main/java/com/mindbadger/wmp/ReadWMPLibrary.java
@@ -13,93 +13,102 @@ import com.mindbadger.library.Track;
 import com.mindbadger.spring.controller.RegisterController;
 
 public class ReadWMPLibrary implements IReadTheWmpLibrary {
-  Logger logger = Logger.getLogger(ReadWMPLibrary.class);
-  
-  public ReadWMPLibrary () {
-    logger.debug("In the constructor of the ReadWMPLibrary");
-  }
-  
-  public Map<String, Artist> readLibrary (JacobAdapter adapter) {
-    
-    Map<String, Artist> library = new HashMap<String, Artist> ();
-    IdGenerator idGenerator = new IdGenerator();
-    
-    Dispatch mediaCollection = adapter.getDispatch("mediaCollection");
-    Dispatch playlist = adapter.call(mediaCollection, "getAll");
-    Variant countVariant = adapter.getVariant(playlist, "count"); 
-    
-    int count = countVariant.getInt();
-    
-    for (int i=0; i < count; i++) {
-      Dispatch playlistItem = adapter.call(playlist, "item", i);
-      Variant mediaType = adapter.getVariant(playlistItem, "getItemInfo", "MediaType");
-      
-      if ("audio".equals(mediaType.toString())) {
-        String trackName = adapter.getVariant(playlistItem, "name").toString();
-        String sourceUrl = adapter.getVariant(playlistItem, "sourceURL").toString ();
-        String artistName = adapter.getVariant(playlistItem, "getItemInfo", "WM/AlbumArtist").toString();
-        String albumName = adapter.getVariant(playlistItem, "getItemInfo", "WM/AlbumTitle").toString();
-        String trackNumberString = adapter.getVariant(playlistItem, "getItemInfo", "WM/TrackNumber").toString();
-        
-        trackName = removeTroublesomeCharacters(trackName);
-        artistName = removeTroublesomeCharacters(artistName);
-        albumName = removeTroublesomeCharacters(albumName);
-        
-        Integer trackNumber = null;
-        try {
-          trackNumber = Integer.parseInt(trackNumberString);
-        } catch (NumberFormatException e) {
-          trackNumber = 0;
-        }
-        
-        Artist artist = library.get(artistName);
-        if (artist == null) {
-          artist = new Artist ();
-          artist.setName(artistName);
-          artist.setId(idGenerator.getNextId());
-          artist.setAlbums(new HashMap<String, Album> ());
-          library.put(artistName, artist);
-        }
-        
-        Album album = artist.getAlbums().get(albumName);
-        if (album == null) {
-          album = new Album ();
-          album.setName(albumName);
-          album.setId(idGenerator.getNextId());
-          album.setTracks(new HashMap<Integer, Track> ());
-          artist.getAlbums().put(albumName, album);
-        }
-        
-        Track track = new Track ();
-        track.setTrackNumber(trackNumber);
-        track.setName(trackName);
-        track.setId(idGenerator.getNextId());
-        track.setFullyQualifiedFileName(sourceUrl);
-        album.getTracks().put(trackNumber, track);
-      }
-    }
-      
-    return library;
-  }
-  
-  public String removeTroublesomeCharacters(String inString)
-  {
-      if (inString == null) return null;
+	Logger logger = Logger.getLogger(ReadWMPLibrary.class);
 
-      StringBuilder newString = new StringBuilder();
-      char ch;
+	public ReadWMPLibrary() {
+		logger.debug("In the constructor of the ReadWMPLibrary");
+	}
 
-      for (int i = 0; i < inString.length(); i++)
-      {
+	public Map<String, Artist> readLibrary(JacobAdapter adapter) {
 
-          ch = inString.charAt(i);
-          // remove any characters outside the valid UTF-8 range as well as all control characters
-          // except tabs and new lines
-          if ((ch < 0x00FD && ch > 0x001F) || ch == '\t' || ch == '\n' || ch == '\r')
-          {
-              newString.append(ch);
-          }
-      }
-      return newString.toString();
-  }
+		logger.debug("In readLibrary method...");
+
+		Map<String, Artist> library = new HashMap<String, Artist>();
+		IdGenerator idGenerator = new IdGenerator();
+
+		Dispatch mediaCollection = adapter.getDispatch("mediaCollection");
+
+		logger.debug("mediaCollection: " + mediaCollection);
+
+		Dispatch playlist = adapter.call(mediaCollection, "getAll");
+
+		logger.debug("getAll: " + playlist);
+
+		Variant countVariant = adapter.getVariant(playlist, "count");
+
+		logger.debug("countVariant: " + countVariant);
+
+		int count = countVariant.getInt();
+
+		for (int i = 0; i < count; i++) {
+			Dispatch playlistItem = adapter.call(playlist, "item", i);
+			Variant mediaType = adapter.getVariant(playlistItem, "getItemInfo", "MediaType");
+
+			if ("audio".equals(mediaType.toString())) {
+				String trackName = adapter.getVariant(playlistItem, "name").toString();
+				String sourceUrl = adapter.getVariant(playlistItem, "sourceURL").toString();
+				String artistName = adapter.getVariant(playlistItem, "getItemInfo", "WM/AlbumArtist").toString();
+				String albumName = adapter.getVariant(playlistItem, "getItemInfo", "WM/AlbumTitle").toString();
+				String trackNumberString = adapter.getVariant(playlistItem, "getItemInfo", "WM/TrackNumber").toString();
+
+				trackName = removeTroublesomeCharacters(trackName);
+				artistName = removeTroublesomeCharacters(artistName);
+				albumName = removeTroublesomeCharacters(albumName);
+
+				Integer trackNumber = null;
+				try {
+					trackNumber = Integer.parseInt(trackNumberString);
+				} catch (NumberFormatException e) {
+					trackNumber = 0;
+				}
+
+				Artist artist = library.get(artistName);
+				if (artist == null) {
+					artist = new Artist();
+					artist.setName(artistName);
+					artist.setId(idGenerator.getNextId());
+					artist.setAlbums(new HashMap<String, Album>());
+					library.put(artistName, artist);
+				}
+
+				Album album = artist.getAlbums().get(albumName);
+				if (album == null) {
+					album = new Album();
+					album.setName(albumName);
+					album.setId(idGenerator.getNextId());
+					album.setTracks(new HashMap<Integer, Track>());
+					artist.getAlbums().put(albumName, album);
+				}
+
+				Track track = new Track();
+				track.setTrackNumber(trackNumber);
+				track.setName(trackName);
+				track.setId(idGenerator.getNextId());
+				track.setFullyQualifiedFileName(sourceUrl);
+				album.getTracks().put(trackNumber, track);
+			}
+		}
+
+		return library;
+	}
+
+	public String removeTroublesomeCharacters(String inString) {
+		if (inString == null)
+			return null;
+
+		StringBuilder newString = new StringBuilder();
+		char ch;
+
+		for (int i = 0; i < inString.length(); i++) {
+
+			ch = inString.charAt(i);
+			// remove any characters outside the valid UTF-8 range as well as all
+			// control characters
+			// except tabs and new lines
+			if ((ch < 0x00FD && ch > 0x001F) || ch == '\t' || ch == '\n' || ch == '\r') {
+				newString.append(ch);
+			}
+		}
+		return newString.toString();
+	}
 }
